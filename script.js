63141a2c8311526e5398307d61964f62f09c2616
ashishsubedi123 <95341740+ashishsubedi123@users.noreply.github.com>
2023-07-06 15:34:47 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//  const body=document.body
+// const div = document.createElement('div')
+// // div.innerText ="hello world";
+// // div.textContent="hello world 2"
+// const strong =document.createElement("strong")
+// strong.innerText ="ashish"
+// body.append(div)
+// // div.innerHTML="<strong> Hello world</strong>"
+
+// body.append(strong)
+//    document.getElementById("hi")
+// const div = document.querySelector("div")
+// const spanhy =document.querySelector('#hi')
+// const spanhello=document.querySelector('#Bye')
+// div.append(spanhy)
+// const body=document.body
+// body.append("hiii")
